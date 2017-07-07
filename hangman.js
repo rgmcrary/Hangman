f8d5961f173ba234257e5716d5b5971af1e06f36
Rich McRary <rgmcrary@gmail.com>
2017-07-07 18:31:26 -0400

    Updated Work
    updated code

--- hangman.js
+++ hangman.js
@@ -1,23 +1,51 @@
 
 // Music Group array ################
 var musicGroups = ["devo", "banarama", "oingo boingo", "david bowie", "dead or alive", "the fixx", 'spandau ballet', "echo and the bunnymen", "the psychedelic furs", "tom tom club"];
+var songs = ["whip it", "venus", "dead man's party", "let's dance", "brand new lover", "one thing leads to another", "poison arrow", "lips like sugar", "pretty in pink", "genius of love"];
 
 
 
-// Answer array ################
-var answers = [];
-for (var i = 0; i < musicGroups.length; i++) {
-    answers[i] = "_";
-}
-
-// var remainingLetters = randGroup.length;
 
 
 
-// Game Loop ################
 document.onkeyup = function () {
-// Random group selection ################
-var randGroup = musicGroups[Math.floor(Math.random() * musicGroups.length)];
+
+    // Random group selection ################
+    var randGroup = musicGroups[Math.floor(Math.random() * musicGroups.length)];
+    console.log(randGroup);
+
+    // Answer array ################
+    var answer = [];
+    for (var i = 0; i < randGroup.length; i++) {
+        answer[i] = "_";
+        console.log(answer);
+    }
+    var remainingLetters = randGroup.length;
+    var guessesLeft = 12;
+
+    // Game Loop ################
+    while (remainingLetters > 0 && guessesLeft > 0) {
+        document.write(answer.join(""));
+        var guess = document.onkeypress;
+        // if (guess === null) {
+        //     break;
+        // }
+        // else 
+        // if (guess.length !== 1) {
+        //     alert("Please enter only 1 letter.");
+        // }
+        // else {
+        for (var j = 0; j < randGroup.length; j++) {
+            if (randGroup[j] === guess) {
+                answer[j] = guess;
+                remainingLetters--;
+            }
+            else {
+                guessesLeft--;
+                // }
+            }
+        }
+    }
 }
 
 
