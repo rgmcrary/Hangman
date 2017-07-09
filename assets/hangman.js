6cbb4e5802b2373e2d92d5834c69b64bdc042932
Rich McRary <rgmcrary@gmail.com>
2017-07-09 18:48:26 -0400

    Final Submission
    Completed converting to object format and renamed html file

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,46 +1,54 @@
-
-// Arrays ################
-var musicGroups = ["devo", "bananarama", "wham", "blondie", "abc", "animotion", 'berlin', "eurythmics", "inxs", "ministry"];
-var songs = ["whip it", "venus", "wake me up", "heart of glass", "the look of love", "obsession", "the metro", "here comes the rain again", "suicide blond", "revenge"];
-var pics = ["devo.jpg", "bananarama.jpg", "wham.jpg", "blondie.jpg", "abc.jpg", "animotion.jpg", 'berlin.jpg', "eurythmics.jpg", "inxs.jpg", "ministry.jpg"];
-var music = ["whip_it.m4a", "venus.m4a", "wake_me_up.m4a", "heart_of_glass.m4a", "look_of_love.m4a", "obsession.m4a", "metro.m4a", "here_comes_the_rain_again.m4a", "suicide_blond.m4a", "revenge.m4a"];
-
-// Var Declarations  ############
-var wins = 0;
-var losses = 0;
+// Var Declarations ##########
 var randGroup;
 var answer;
 var remainingLetters;
 var guessesLeft;
 var guesses;
 var artistSong;
+var audioPlaying;
 
 
+
+var musicGroups = [
+    { name: "devo", song: "whip it", pic: "devo.jpg", music: "whip_it.m4a" },
+    { name: "bananarama", song: "venus", pic: "bananarama.jpg", music: "venus.m4a" },
+    { name: "wham", song: "wake me up", pic: "wham.jpg", music: "wake_me_up.m4a" },
+    { name: "blondie", song: "heart of glass", pic: "blondie.jpg", music: "heart_of_glass.m4a" },
+    { name: "abc", song: "the look of love", pic: "abc.jpg", music: "look_of_love.m4a" },
+    { name: "animotion", song: "obsession", pic: "animotion.jpg", music: "obsession.m4a" },
+    { name: "berlin", song: "the metro", pic: "berlin.jpg", music: "metro.m4a" },
+    { name: "eurythmics", song: "here comes the rain again", pic: "eurythmics.jpg", music: "here_comes_the_rain_again.m4a" },
+    { name: "inxs", song: "suicide blond", pic: "inxs.jpg", music: "suicide_blond.m4a" },
+    { name: "ministry", song: "revenge", pic: "ministry.jpg", music: "revenge.m4a" },
+];
+
+var wins = 0;
+var losses = 0;
+
+var body = document.body;
+document.body.addEventListener("keyup", takeTurn);
 
 
 function gameStart() {
+
     // Random group selection ################
     document.getElementById("restartButton").style.display = "none";
     randGroup = musicGroups[Math.floor(Math.random() * musicGroups.length)];
-    console.log(randGroup);
 
     // Answer array ################
     answer = [];
-    for (var i = 0; i < randGroup.length; i++) {
+    for (var i = 0; i < randGroup.name.length; i++) {
         answer[i] = "_";
     }
-    remainingLetters = randGroup.length;
+    remainingLetters = randGroup.name.length;
     guessesLeft = 12;
 
     // Guesses array ############
     guesses = [];
 
     //  Artist/Song Reveal ###########
-    artistSong = randGroup + " - " + songs[musicGroups.indexOf(randGroup)];
-
-    // Image Reveal ##############
-    // var pics = pics[musicGroups.indexOf(randGroup)];
-
+    artistSong = randGroup.name + " - " + randGroup.song;
+ 
     document.getElementById("gameBoard").innerHTML = answer.join(" ");
     document.getElementById("guessesLeft").innerHTML = guessesLeft;
     document.getElementById("lettersGuessed").innerHTML = guesses;
@@ -48,6 +56,7 @@ function gameStart() {
     document.getElementById("reveal").innerHTML = "";
     document.getElementById("bandPic").innerHTML = "";
     document.getElementById("result").appendChild = "";
+    audioPlaying = false;
 }
 
 
@@ -56,8 +65,8 @@ function takeTurn(event) {
     // Game Loop ################
     var guess = event.key;
     if (remainingLetters > 0 && guessesLeft > 0 && guesses.indexOf(guess) === -1) {
-        for (var j = 0; j < randGroup.length; j++) {
-            if (randGroup[j] === guess) {
+        for (var j = 0; j < randGroup.name.length; j++) {
+            if (randGroup.name[j] === guess) {
                 answer[j] = guess;
                 remainingLetters--;
             }
@@ -71,12 +80,12 @@ function takeTurn(event) {
         document.getElementById("lettersGuessed").innerHTML = guesses.join(" ").toUpperCase();
     }
 
-    if (remainingLetters === 0 && guessesLeft > 0) {
+    if (remainingLetters === 0 && guessesLeft > 0 && audioPlaying === false) {
         wins++;
         updateDisplay("YOU DID IT!!!", "win");
     }
 
-    if (remainingLetters > 0 && guessesLeft === 0) {
+    if (remainingLetters > 0 && guessesLeft === 0 && audioPlaying === false) {
         losses++;
         updateDisplay("TOUGH LUCK...BETTER LUCK NEXT TIME", "loss");
     }
@@ -85,39 +94,14 @@ function takeTurn(event) {
 function updateDisplay(resultText, updateType) {
     document.getElementById("result").innerHTML = resultText;
     document.getElementById("reveal").innerHTML = artistSong.toUpperCase();
-    document.getElementById("bandPic").innerHTML = "<img src = 'assets/images/" + pics[musicGroups.indexOf(randGroup)] + "'>";
-    var audio = new Audio("assets/audio/" + music[musicGroups.indexOf(randGroup)]);
+    document.getElementById("bandPic").innerHTML = "<img src = 'assets/images/" + randGroup.pic + "'>";
+    var audio = new Audio("assets/audio/" + randGroup.music);
     document.getElementById("restartButton").style.display = "block";
     if (updateType === "win") {
         document.getElementById("wins").innerHTML = wins;
-        // var audio = new Audio("assets/audio/" + celebration.wav");
     } else {
         document.getElementById("losses").innerHTML = losses;
-        // var audio = new Audio("assets/audio/boo.wav");
     }
+    audioPlaying = true;
     audio.play();
 }
-
-
-
-
-
-
-
-
-// Press any key to begin
-//     Random generate word from Array
-//     Provide blanks equal to word length 
-//     display letters chosen incorrectly
-//     populate correctly corresponding to place in word in blanks
-//     allow for 12 guesses
-//         once letter is chosen, choosing again doesnt detract from guess count
-//         correct guess doesnt detract from guess count 
-//     game ends
-//         if wrong, play sound and end game 
-//         if correct, play sound and end game 
-//             play artist name and a song
-// Scoring
-    // # of wins 
-    // # of loses 
-
